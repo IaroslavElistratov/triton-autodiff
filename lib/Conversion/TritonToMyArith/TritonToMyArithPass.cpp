1f2cdac068c11d45087fa9cfdd9ef2e8ceda9d97
IaroslavElistratov <iaroslav.elistratov@gmail.com>
2025-03-01 07:11:00 +0300

    add comments

--- lib/Conversion/TritonToMyArith/TritonToMyArithPass.cpp
+++ lib/Conversion/TritonToMyArith/TritonToMyArithPass.cpp
@@ -21,9 +21,16 @@ namespace {
 // TritonToMyArithPass.cpp
 struct ConvertTritonToMyArith
     : public impl::ConvertTritonToMyArithBase<ConvertTritonToMyArith> {
+
   using ConvertTritonToMyArithBase::ConvertTritonToMyArithBase;
+
+  // main function
   void runOnOperation() override {
+    // grab the module (IOW root) op
     auto mod = getOperation();
+    // walk this recursively structred IR, and call rewriteSplatAddOp
+    // only on "triton::FuncOp" -- which is the function op which encapsulates
+    // our entire program
     mod->walk([&](triton::FuncOp func) {
       rewriteSplatAddOp(func);
     });
@@ -31,23 +38,34 @@ struct ConvertTritonToMyArith
 
 
   void rewriteAddFOp(arith::AddFOp addFOp, Value tensorSrc, Value scalarSrc) {
+    // instantiate OpBuilder
     OpBuilder b(addFOp);
 
+    // figure out which one is the tensor operand,
+    // we already have the scalar operand as a parameter to this fn
     auto tensorOpnd =
         addFOp.getLhs() == tensorSrc ? addFOp.getRhs() : addFOp.getLhs();
+
+    // create AddTensorScalarOp
     auto newOp = b.create<myarith::AddTensorScalarOp>(
         addFOp.getLoc(), tensorOpnd.getType(), tensorOpnd, scalarSrc);
 
+    // replace the use of the result of "arith::AddFOp" with the
+    // result of "myarith::AddTensorScalarOp"
     addFOp->replaceAllUsesWith(newOp);
   };
 
   void rewriteSplatAddOp(triton::FuncOp func) {
+    // walk the IR, pattern match SplatOp
     func->walk([&](triton::SplatOp splatOp) {
       auto src = splatOp.getSrc();
       auto res = splatOp.getResult();
 
+      // iterate over the users of the result of the SplatOp
       for (auto user : res.getUsers()) {
+        // for each user, check if the user is the "arith::AddFOp"
         if (auto addOp = dyn_cast<arith::AddFOp>(user)) {
+          // we found the pattern we're looking for, so re-write this op
           rewriteAddFOp(addOp, res, src);
         }
       }
