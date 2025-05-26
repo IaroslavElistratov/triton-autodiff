33cb8140b89594b711b2e7c4ef8a135aa960a80e
IaroslavElistratov <iaroslav.elistratov@gmail.com>
2025-05-26 22:54:07 +0300

    optional tests

--- third_party/autodiff/test/run_all_tests.py
+++ third_party/autodiff/test/run_all_tests.py
@@ -1,21 +1,27 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import subprocess
 
 if __name__ == "__main__":
 
-  for test_name in ["add", "add-mul", "div", "add-mul-div", "math-ops",
-                    "multiblock_add-mul", "mask_multiblock_add-mul",
-                    "2d_dot", "multi-use",
-                    "for-loop", "for-loop-mm_static-bounds",
-                    "LOCAL_for-loop-mm-static-bounds_simple", "LOCAL_for-loop-mm-static-bounds_actual",
-                    "flash_attention_v2", "layernorm"]:
-  # for test_name in ["flash_attention_v2"]:
+  tests = [
+    "add", "add-mul", "div", "add-mul-div", "math-ops",
+    "multiblock_add-mul", "mask_multiblock_add-mul",
+    "2d_dot", "multi-use",
+    "for-loop", "for-loop-mm_static-bounds",
+    "flash_attention_v2", "layernorm"
+  ]
 
+  if int(os.environ.get('EXTRA', 0)):
+    tests.extend([
+      "LOCAL_for-loop-mm-static-bounds_simple",
+      "LOCAL_for-loop-mm-static-bounds_actual"
+    ])
+
+  for test_name in tests:
     print("~" * 20 + f" Running {test_name} " + "~" * 20)
-
     # produce fwd and bwd ttir; and compare outs and grads with torch
     subprocess.run([sys.executable, "run.py"], cwd=test_name)
-
   print("~" * 50)
