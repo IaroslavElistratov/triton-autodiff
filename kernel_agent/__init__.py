3c3a356939292056a540d434214d3fb33a8d0b8f
IaroslavElistratov <iaroslav.elistratov@gmail.com>
2025-11-03 13:20:32 +0000

    [mv][wip] make kernel_agent independant from triton_autodiff

---
+++ kernel_agent/__init__.py
@@ -0,0 +1,11 @@
+from api import (
+    autodiff,
+    StubOverrideDCK,
+    record_autodiff_artifacts,
+    autodiff_overwrite_fp,
+    get_last_bwd_fp,
+)
+
+__all__ = ["autodiff", "StubOverrideDCK", "record_autodiff_artifacts", "autodiff_overwrite_fp", "get_last_bwd_fp"]
+
+
