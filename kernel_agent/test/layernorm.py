73149f1a06b1553929dbfc821b8be5ab1b18af95
IaroslavElistratov <iaroslav.elistratov@gmail.com>
2025-11-08 05:37:25 +0000

    [test] update layernorm

--- kernel_agent/test/layernorm.py
+++ kernel_agent/test/layernorm.py
@@ -9,10 +9,11 @@ from kernel_agent.autodiff import autodiff
 # # torch.set_printoptions(sci_mode=False, linewidth=1000)
 # DEVICE = torch.device("cuda:0")
 
-torch.manual_seed(0)
-DEVICE = torch.device("cuda:0")
 
 # NOTE: copied from official triton tutorial -- https://triton-lang.org/main/getting-started/tutorials/05-layer-norm.html#sphx-glr-getting-started-tutorials-05-layer-norm-py
+
+
+
 
 @triton.jit
 def _layer_norm_fwd_fused(
@@ -22,16 +23,15 @@ def _layer_norm_fwd_fused(
     B,  # pointer to the biases
     Mean,  # pointer to the mean
     Rstd,  # pointer to the 1/std
-    stride: tl.constexpr,  # how much to increase the pointer when moving by 1 row
-    N: tl.constexpr,  # number of columns in X
-    eps: tl.constexpr,  # epsilon to avoid division by zero
+    stride,  # how much to increase the pointer when moving by 1 row
+    N,  # number of columns in X
+    eps,  # epsilon to avoid division by zero
     BLOCK_SIZE: tl.constexpr,
 ):
     # Map the program id to the row of X and Y it should compute.
     row = tl.program_id(0)
     Y += row * stride
     X += row * stride
-
     # Compute mean
     mean = 0
     _mean = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
@@ -40,7 +40,6 @@ def _layer_norm_fwd_fused(
         a = tl.load(X + cols, mask=cols < N, other=0.).to(tl.float32)
         _mean += a
     mean = tl.sum(_mean, axis=0) / N
-
     # Compute variance
     _var = tl.zeros([BLOCK_SIZE], dtype=tl.float32)
     for off in range(0, N, BLOCK_SIZE):
@@ -48,14 +47,11 @@ def _layer_norm_fwd_fused(
         x = tl.load(X + cols, mask=cols < N, other=0.).to(tl.float32)
         x = tl.where(cols < N, x - mean, 0.)
         _var += x * x
-
     var = tl.sum(_var, axis=0) / N
     rstd = 1 / tl.sqrt(var + eps)
-
     # Write mean / rstd
     tl.store(Mean + row, mean)
     tl.store(Rstd + row, rstd)
-
     # Normalize and apply linear transformation
     for off in range(0, N, BLOCK_SIZE):
         cols = off + tl.arange(0, BLOCK_SIZE)
@@ -68,14 +64,15 @@ def _layer_norm_fwd_fused(
         # Write output
         tl.store(Y + cols, y, mask=mask)
 
-@autodiff(idxs_buffers=(0, 1, 2))
-def stub(x, weight, bias, eps=1e-5):
+
+# todo-now: add decorator to specify that some outputs don't require grads
+@autodiff(idxs_buffers=(0, 2, 3))
+def forward_layernorm(x, normalized_shape, weight, bias, eps):
     # allocate output
     y = torch.empty_like(x)
     # reshape input data into 2D tensor
     x_arg = x.reshape(-1, x.shape[-1])
     M, N = x_arg.shape
-    # some input types to the fn are float16 and some are float32
     mean = torch.empty((M, ), dtype=torch.float32, device=x.device)
     rstd = torch.empty((M, ), dtype=torch.float32, device=x.device)
     # Less than 64KB per feature: enqueue fused kernel
@@ -86,16 +83,11 @@ def stub(x, weight, bias, eps=1e-5):
     # heuristics for number of warps
     num_warps = min(max(BLOCK_SIZE // 256, 1), 8)
     # enqueue kernel
-    grid = (M, )
-    _layer_norm_fwd_fused[grid](  #
-        x, y, weight, bias, mean, rstd,  #
+    _layer_norm_fwd_fused[(M, )](  #
+        x_arg, y, weight, bias, mean, rstd,  #
         x_arg.stride(0), N, eps,  #
-        # old-todo: commenting out "num_warps=num_warps, num_ctas=1" solves the error!
-        BLOCK_SIZE=BLOCK_SIZE #, num_warps=num_warps, num_ctas=1
-    )
-    return y
-
-
+        BLOCK_SIZE=BLOCK_SIZE, num_warps=num_warps, num_ctas=1)
+    return y, mean, rstd, num_warps, BLOCK_SIZE
 
 
 SWEEP = [
@@ -129,4 +121,4 @@ def flops(dims, mode):
 
 def setup():
     (x, weight, bias), _ = make_args(SWEEP[0])
-    stub(x, weight, bias)
+    forward_layernorm(x, weight, bias)
